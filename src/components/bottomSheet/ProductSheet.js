7998e153d603405e635f26c55d9b9cc89ebccf17
james-muriithi <muriithijames556@gmail.com>
2020-07-08 16:59:08 +0300

    added product bottom sheet

---
+++ src/components/bottomSheet/ProductSheet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SwipeableBottomSheet from 'react-swipeable-bottom-sheet';
+
+export default function ProductSheet(props) {
+    return (
+        <SwipeableBottomSheet overflowHeight={0} {...props} style={{ zIndex: '100' }} bodyStyle={{
+            borderTopLeftRadius: "15px",
+            borderTopRightRadius: "15px"}} >
+            <div style={{ maxHeight: '90%', minHeight: "240px", padding: "20px"}}>
+                {props.children}
+            </div>
+        </SwipeableBottomSheet>
+    )
+}
--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Col } from "reactstrap";
 import { Link } from "gatsby";
 
 import './ProductCard.css';
 import Like from '../favourite/Like';
-import placeholder from "../../images/products/placeholder.png";
 import 'lazysizes';
 import 'lazysizes/plugins/attrchange/ls.attrchange';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import ProductSheet from '../bottomSheet/ProductSheet';
 
 export default function ProductCard({product, ...props}) {
+    const [open, setOpen] = useState(false);
+
+    const onClick = (e) =>{
+        e && e.preventDefault();
+        setOpen(prevState => !prevState);
+    }
+
     return (
         <Col {...props}>
             <div className="product-item">
                 <div className="pi-pic">
-                    <img data-src={product.src ? product.src : placeholder} alt={product.name} src="https://res.cloudinary.com/james-m/image/upload/a_hflip,c_pad,h_303,w_270/v1594207296/1612585_j6ym3x.webp"  className="lazyload" />
+                    <img data-src={product.src} alt={product.name} src="https://res.cloudinary.com/james-m/image/upload/a_hflip,c_pad,h_303,w_270/v1594207296/1612585_j6ym3x.webp"  className="lazyload" />
                     <div className="icon like">
                         <Like />
                     </div>
@@ -23,7 +30,7 @@ export default function ProductCard({product, ...props}) {
                             <Link to="#"><i className="fa fa-shopping-bag"></i></Link>
                         </li>
                         <li className="quick-view d-md-none">
-                            <Link to="#">+ View</Link>
+                            <Link to="#" onClick={onClick}>+ View</Link>
                         </li>
                         <li className="w-icon">
                             <Link to="#"><i className="fa fa-share"></i></Link>
@@ -31,7 +38,6 @@ export default function ProductCard({product, ...props}) {
                     </ul>
                 </div>
                 <div className="pi-text">
-                    <div className="catagory-name">Towel</div>
                     <Link to="#">
                         <h5>{product.name}</h5>
                     </Link>
@@ -40,6 +46,7 @@ export default function ProductCard({product, ...props}) {
                     </div>
                 </div>
             </div>
+            <ProductSheet open={open} onChange={onClick} />
         </Col>
     )
 }
